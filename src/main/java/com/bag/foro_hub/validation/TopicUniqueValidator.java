993c286864c5a7bf3c88ad84a183220b506afefd
Brayam Ayala <b.ayalaga06@gmail.com>
2025-12-16 19:10:28 -0500

    Add Topic validation logic with a unique title and message constraint

---
+++ src/main/java/com/bag/foro_hub/validation/TopicUniqueValidator.java
@@ -0,0 +1,20 @@
+package com.bag.foro_hub.validation;
+
+import com.bag.foro_hub.model.dto.request.CreateTopicRequest;
+import com.bag.foro_hub.repository.TopicRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TopicUniqueValidator implements TopicValidator {
+
+  private final TopicRepository topicRepository;
+
+  @Override
+  public void validate(CreateTopicRequest request) {
+    if (topicRepository.existsByTitleAndMessage(request.title(), request.message())) {
+      throw new RuntimeException("Ya existe un tema con ese titulo y mensaje");
+    }
+  }
+}
