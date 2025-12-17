c17853f65851f41e8266657fbb6dd17263b39876
Brayam Ayala <b.ayalaga06@gmail.com>
2025-12-16 19:09:58 -0500

    Add JPA repositories for Course, Topic, and User entities

---
+++ src/main/java/com/bag/foro_hub/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.bag.foro_hub.repository;
+
+import com.bag.foro_hub.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
