4f230c398beb65007b0065e263f944e105b63bdc
Brayam Ayala <b.ayalaga06@gmail.com>
2025-12-17 00:30:56 -0500

    Refactor exception handling in Topic validation and service layers, update CreateTopicRequest annotations for consistency

--- src/main/java/com/bag/foro_hub/validation/TopicUniqueValidator.java
+++ src/main/java/com/bag/foro_hub/validation/TopicUniqueValidator.java
@@ -1,5 +1,6 @@
 package com.bag.foro_hub.validation;
 
+import com.bag.foro_hub.exceptions.ValidatorException;
 import com.bag.foro_hub.model.dto.request.CreateTopicRequest;
 import com.bag.foro_hub.repository.TopicRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,7 @@ public class TopicUniqueValidator implements TopicValidator {
   @Override
   public void validate(CreateTopicRequest request) {
     if (topicRepository.existsByTitleAndMessage(request.title(), request.message())) {
-      throw new RuntimeException("Ya existe un tema con ese titulo y mensaje");
+      throw new ValidatorException("Ya existe un tema con ese titulo y mensaje");
     }
   }
 }
