6cbf046f2312cd613cfb50e669f56c59b9ace51d
Brayam Ayala <b.ayalaga06@gmail.com>
2025-12-19 20:40:03 -0500

    Add markAsSolution method with ownership validation in ReplyService; update save method with authId usage and transactional annotations

--- src/main/java/com/bag/foro_hub/service/ReplyServiceImpl.java
+++ src/main/java/com/bag/foro_hub/service/ReplyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bag.foro_hub.service;
 
 import com.bag.foro_hub.exceptions.ReplyNotFoundException;
+import com.bag.foro_hub.exceptions.RoleAccessDeniedException;
 import com.bag.foro_hub.exceptions.TopicNotFoundException;
 import com.bag.foro_hub.exceptions.UserNotFoundException;
 import com.bag.foro_hub.mapper.ReplyMapper;
@@ -12,8 +13,10 @@ import com.bag.foro_hub.model.entity.User;
 import com.bag.foro_hub.repository.ReplyRepository;
 import com.bag.foro_hub.repository.TopicRepository;
 import com.bag.foro_hub.repository.UserRepository;
+import com.bag.foro_hub.utils.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -27,11 +30,12 @@ public class ReplyServiceImpl implements ReplyService {
   private final ReplyMapper replyMapper;
 
   @Override
-  public ReplyResponse save(CreateReplyRequest request) {
+  @Transactional
+  public ReplyResponse save(CreateReplyRequest request, Long authId) {
     Topic topic =
         topicRepository.findById(request.topicId()).orElseThrow(TopicNotFoundException::new);
 
-    User user = userRepository.findById(request.userId()).orElseThrow(UserNotFoundException::new);
+    User user = userRepository.findById(authId).orElseThrow(UserNotFoundException::new);
 
     Reply reply = new Reply();
     reply.setMessage(request.message());
@@ -42,15 +46,44 @@ public class ReplyServiceImpl implements ReplyService {
   }
 
   @Override
+  @Transactional(readOnly = true)
   public List<ReplyResponse> findAll() {
     return replyRepository.findAll().stream().map(replyMapper::toReplyResponse).toList();
   }
 
   @Override
+  @Transactional(readOnly = true)
   public ReplyResponse findById(Long id) {
     return replyRepository
         .findById(id)
         .map(replyMapper::toReplyResponse)
         .orElseThrow(ReplyNotFoundException::new);
+  }
+
+  @Override
+  @Transactional
+  public void markAsSolution(Long replyId, Long authid) {
+    Reply reply = replyRepository.findById(replyId).orElseThrow(ReplyNotFoundException::new);
+
+    Topic topic = reply.getTopic();
+
+    boolean isOwner = topic.belongsToUser(authid);
+
+    if (!isOwner) {
+      throw new RoleAccessDeniedException("You are not the owner of this topic");
+    }
+
+    replyRepository
+        .findByTopicIdAndIsSolutionTrue(topic.getId())
+        .ifPresent(
+            currentSolution -> {
+              currentSolution.setIsSolution(false);
+              replyRepository.save(currentSolution);
+            });
+
+    reply.setIsSolution(true);
+    topic.setStatus(Status.SOLVED);
+    replyRepository.save(reply);
+    topicRepository.save(topic);
   }
 }
