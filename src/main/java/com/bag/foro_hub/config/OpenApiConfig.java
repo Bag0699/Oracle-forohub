712a2e05761b0fe16b18f203e3d349213598f315
Brayam Ayala <b.ayalaga06@gmail.com>
2025-12-21 19:26:04 -0500

    Add OpenAPI configuration with JWT authentication support; include springdoc dependency in pom.xml

---
+++ src/main/java/com/bag/foro_hub/config/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.bag.foro_hub.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("For Hub API")
+                .version("1.0")
+                .contact(new Contact()
+                        .name("Brayan Ayala")
+                        .url("https://github.com/bag0699"))
+                .description("API para la gestión de tópicos y preguntas en el foro"))
+        .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "bearer-key",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+  }
+}
