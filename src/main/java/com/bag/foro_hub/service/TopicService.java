c7bc9d9717fa5854b4b7f983932e0fa63f926bb1
Brayam Ayala <b.ayalaga06@gmail.com>
2025-12-24 13:51:30 -0500

    Paginate `findAll` method in TopicService, add `findAllByCourseAndYear` method for filtering topics by course and year

--- src/main/java/com/bag/foro_hub/service/TopicService.java
+++ src/main/java/com/bag/foro_hub/service/TopicService.java
@@ -3,18 +3,19 @@ package com.bag.foro_hub.service;
 import com.bag.foro_hub.model.dto.request.CreateTopicRequest;
 import com.bag.foro_hub.model.dto.request.UpdateTopicRequest;
 import com.bag.foro_hub.model.dto.response.TopicResponse;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 public interface TopicService {
 
   TopicResponse save(CreateTopicRequest request, Long authId);
 
-  List<TopicResponse> findAll();
+  Page<TopicResponse> findAll(int page, int size);
+
+  Page<TopicResponse> findAllByCourseAndYear(Long courseId, int year, int page, int size);
 
   TopicResponse findById(Long id);
 
-  TopicResponse update(Long id, UpdateTopicRequest request , Long authId);
+  TopicResponse update(Long id, UpdateTopicRequest request, Long authId);
 
   void deleteById(Long id, Long authId);
 }
